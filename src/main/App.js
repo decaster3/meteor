097164eae31d77648b31e73d90d793f13392703d
Rinat Katipov <rinat777hatipov@gmail.com>
2017-09-04 19:35:29 +0300

    added dign in and sign up pages with basic functions

--- src/main/App.js
+++ src/main/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 import MainFirstPage from '../mainPage/components/MainFirstPage'
+import MainComponent from '../firstPage/components/MainComponent'
+import SignUpComponent from '../auth/components/SignUpComponent'
+import SignInComponent from '../auth/components/SignInComponent'
+import { Router, Route, browserHistory } from 'react-router'
 class App extends Component{
 
   render(){
       return (
-        <MainFirstPage/>
+        <Router history={browserHistory}>
+          <Route path='/' component={MainComponent} />
+          <Route path='/menu' component={MainFirstPage} />
+          <Route path='/sign_up' component={SignUpComponent} />
+          <Route path='/sign_in' component={SignInComponent} />
+        </Router>
       );
   }
 }
