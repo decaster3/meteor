10182f1327aa3306ddb18d8b4b6fbd295df5c588
Rinat Katipov <rinat777hatipov@gmail.com>
2017-09-09 01:03:04 +0300

    added cart methods

--- src/cart/cartMethods.js
+++ src/cart/cartMethods.js
@@ -1,49 +1,88 @@
-getCoockies(){
-  const cookiess = new Cookies();
-  var cont = cookiess.get('cart');
-  var contt = {
-    quantity: cont.quantity + 1,
-  };
-  console.log(contt);
-  cookiess.set('cart', contt, {path: '/'});
+mcompareTopings(topings1,topings2){
+  var arr1 = [], arr2 = [];
+
+  for(var i = 0; i < topings1.length;i++){
+    arr1.push(topings1[i].toping_id)
+  }
+
+  for(var j = 0; j < topings2.length;j++){
+    arr2.push(topings2[j].toping_id)
+  }
+
+  for (var i = 0; i < arr2.length; i++){
+    if (!arr1.includes(arr2[i])){
+      return false;
+    }
+  }
+
+  for (var i = 0; i < arr1.length; i++){
+    if (!arr2.includes(arr1[i])){
+      return false;
+    }
+  }
+  return true;
 }
-addProductToCart(){
-  const coockis = new Coockies();
-  coockis.set('cart', {
 
-  },{path: '/'});
+getCart(){
+  const cookies = new Cookies();
+  console.log(cookies.get('cart'));
 }
-addProductToCart(){
-  const coockies = new Cookies();
-  coockies.set('cart',{
-      quantityproducts: int,
-      products: [
-        {
-          category: string,
-          name: string,
-          description: string,
-          price: int,
-          aditional_info_bool: bool,
-          aditional_info: [string, string],
-          quantity_topings: int,
-          topings: [{
-            name: string,
-            count: int,
-            price_per_one: int,
-            price_total: int,
-            weight_per_one: int,
-            weight_total: int
-          },
-          {
 
-          }]
+addProductToCart(product){
+  const cookies = new Cookies();
+  var current_cart = cookies.get('cart');
+    for(var i = 0; i < current_cart.quantityproducts; i++){
+      if (product.product_id == current_cart.products[i].product_id &&
+                                    this.compareTopings(product.topings,current_cart.products[i].topings)){
+        current_cart.products[i].quantity += 1;
+        current_cart.quantityproducts +=1;
+        cookies.set('cart', current_cart, {path: '/'});
+        return;
+      }
+    }
+    cookies.set('cart', {
+      quantityproducts: current_cart.quantityproducts + 1,
+      products: [...current_cart.products,{
+        quantity: 1,
+        product_id: product.product_id,
+        topings: product.topings
+      }]}
+      ,{path: '/'});
+}
 
-        },
-        {
+create_cart(){
+  const cookies = new Coockies();
+  cookies.set('cart', {
+    quantityproducts: 0,
+    products:[]
+  }
+  ,{path:'/'})
+}
 
+removeProductFromCart(product){
+  const cookies = new Cookies();
+  var current_cart = cookies.get('cart');
+    if (current_cart.quantityproducts > 0){
+      for(var i = 0; i < current_cart.quantityproducts; i++){
+        if (product.product_id == current_cart.products[i].product_id &&
+                                      this.compareTopings(product.topings,current_cart.products[i].topings) ){
+          if (current_cart.products[i].quantity > 1){
+            current_cart.products[i].quantity -= 1;
+            current_cart.quantityproducts -=1;
+            cookies.set('cart', current_cart, {path: '/'});
+            return;
+          }
+          else{
+            current_cart.products.splice(i,1);
+            current_cart.quantityproducts -=1;
+            cookies.set('cart', current_cart, {path: '/'});
+            return;
+          }
         }
-      ]
-
-  },
-  {path:'/'});
+      }
+   }else {
+     console.log("Thre is no products in cart");
+     return;
+   }
+   console.log("There is no such product in cart");
 }
